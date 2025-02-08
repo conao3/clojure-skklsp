1047dd549a2f8ec5562334176f19c5168f71c996
conao3 <conao3@gmail.com>
2025-02-08 09:42:39 +0900

    commit

--- src/skklsp/kana.clj
+++ src/skklsp/kana.clj
@@ -3,6 +3,14 @@
 (defn compile [obj]
   ((:compiler obj) obj))
 
+(defn asis-compiler [obj]
+  obj)
+
+(defn merge-compiler [obj]
+  (->> obj
+       (map compile)
+       merge))
+
 (defn table-compiler [obj]
   (->> (:y obj)
        (mapcat (fn [[key val]]
@@ -10,25 +18,64 @@
        (filter second)
        (into {})))
 
-(def basic-rule
-  {:compiler table-compiler
-   :x ["" "k" "s" "t" "n" "h" "f" "m" "y" "r" "w"]
-   :y {"a" ["あ" "か" "さ" "た" "な" "は" "ふぁ" "ま" "や" "ら" "わ"]
-       "i" ["い" "き" "し" "ち" "に" "ひ" "ふぃ" "み" nil "り" nil]
-       "u" ["う" "く" "す" "つ" "ぬ" "ふ" "ふ" "む" "ゆ" "る" nil]
-       "e" ["え" "け" "せ" "て" "ね" "へ" "ふぇ" nil "め" "れ" nil]
-       "o" ["お" "こ" "そ" "と" "の" "ほ" "ふぉ" "も" "よ" "ろ" "を"]}})
+(defn compile-with-basic-rule [obj_]
+  (let [obj (-> obj_
+                (cond-> (string? (:z obj_))
+                  (update :z #(repeat (count (:y1 obj_)) %))))
+        basic-rule (skklsp.kana/compile skklsp.kana/basic-rule)]
+    (->> [(:y1 obj) (:y2 obj) (:z obj)]
+         (apply mapcat (fn [yi1 yi2 z]
+                         (map (fn [x] [(str x yi1) (get basic-rule (str x yi2)) z])
+                              (:x obj))))
+         (filter second)
+         (map (fn [[k v z]] [k (str v z)]))
+         (into {}))))
 
-;;  	k 	s 	t 	n 	h 	f 	m 	y 	r 	w
-;; z 	かん 	さん 	たん 	なん 	はん 	ふぁん 	まん 	やん 	らん 	わん
-;; n 	かん 	さん 	たん 	ん 	はん 	ふぁん 	* 	やん 	らん 	わん
-;; k 	きん 	しん 	ちん 	にん 	ひん 	ふぃん 	みん 		りん 	うぃん
-;; j 	くん 	すん 	つん 	ぬん 	ふん 	ふん 	むん 	ゆん 	るん
-;; d 	けん 	せん 	てん 	ねん 	へん 	ふぇん 	めん 		れん 	うぇん
-;; l 	こん 	そん 	とん 	のん 	ほん 	ふぉん 	もん 	よん 	ろん 	うぉん
+(def basic-rule
+  {:compiler merge-compiler
+   :children
+   [{:compiler table-compiler
+     :x ["" "k" "s" "t" "n" "h" "f" "m" "y" "r" "w"]
+     :y {"a" ["あ" "か" "さ" "た" "な" "は" "ふぁ" "ま" "や" "ら" "わ"]
+         "i" ["い" "き" "し" "ち" "に" "ひ" "ふぃ" "み" nil "り" nil]
+         "u" ["う" "く" "す" "つ" "ぬ" "ふ" "ふ" "む" "ゆ" "る" nil]
+         "e" ["え" "け" "せ" "て" "ね" "へ" "ふぇ" nil "め" "れ" nil]
+         "o" ["お" "こ" "そ" "と" "の" "ほ" "ふぉ" "も" "よ" "ろ" "を"]}}
+    {:compiler table-compiler
+     :x ["g" "z" "d" "b" "p"]
+     :y {"a" ["が" "ざ" "だ" "ば" "ぱ"]
+         "i" ["ぎ" "じ" "ぢ" "び" "ぴ"]
+         "u" ["ぐ" "ず" "づ" "ぶ" "ぷ"]
+         "e" ["げ" "ぜ" "で" "べ" "ぺ"]
+         "o" ["ご" "ぞ" "ど" "ぼ" "ぽ"]}}
+    {:compiler asis-compiler
+     :x {"zc" "ざ"}}]})
 
 (def azik-youon-rule
-  {:compiler compile-with-basic-rule
-   :x ["" "k" "s" "t" "n" "h" "f" "m" "y" "r" "w"]
-   :y ["z" "n" "k" "j" "d" "l"]}
-)
+  {:compiler merge-compiler
+   :children
+   [{:compiler compile-with-basic-rule
+     :x ["k" "s" "t" "n" "h" "f" "m" "y" "r" "w" "g" "z" "d" "b" "p"]
+     :y1 ["z" "n" "k" "j" "d" "l"]
+     :y2 ["a" "a" "i" "u" "e" "o"]
+     :z "ん"}
+    {:compiler asis-compiler
+     :x {"nn" "ん"
+         "wn" "わん"
+         "wk" "うぃん"
+         "wd" "うぇん"
+         "wl" "うぉん"}}]})
+
+(def azik-niju-boin-rule
+  {:compiler merge-compiler
+   :children
+   [{:compiler compile-with-basic-rule
+     :x ["k" "s" "t" "n" "h" "f" "m" "y" "r" "w" "g" "z" "d" "b" "p"]
+     :y1 ["q" "h" "w" "p"]
+     :y2 ["a" "u" "e" "o"]
+     :z ["い" "う" "い" "う"]}
+    {:compiler asis-compiler
+     :x {"fp" "ふぉー"
+         "wp" "うぉー"
+         "zv" "ざい"
+         "zx" "ぜい"}}]})
